0f78d955f726f3081780961112bf07ec34dbe31b
dark-eraser <31793252+dark-eraser@users.noreply.github.com>
2023-11-08 17:50:40 +0100

    fixed api key

--- main.py
+++ main.py
@@ -3,38 +3,39 @@ import googlemaps
 import datetime
 import time
 from argparse import ArgumentParser
+import os
 
-def compute_itinerary(api_key=""):
-  with open('credentials.json') as f:
-      data = json.load(f)
-  if data['MAPS_API_KEY']:
-    api_key = data['MAPS_API_KEY']
-  else:
-    api_key = api_key
-  print(api_key)
-  gmaps = googlemaps.Client(key=api_key)
-  origin_location = "9H4X+R6 Zürich"
-  directions_to_eth = gmaps.directions(origin_location, "ETH Zurich", mode="transit", departure_time=datetime.datetime.now())
-  # write directions to file correclty formatted
-  with open('directions_to_eth.json', 'w') as outfile:
-      json.dump(directions_to_eth, outfile, indent=4, sort_keys=True)
+def compute_itinerary(api_key_arg=""):
+    # check first if credentials file is available
+    if os.path.exists('credentials.json'): 
+        with open('credentials.json') as f:
+            data = json.load(f)
+        api_key = data['MAPS_API_KEY']
+    else:
+        api_key = api_key_arg
+    gmaps = googlemaps.Client(key=api_key)
+    origin_location = "9H4X+R6 Zürich"
+    directions_to_eth = gmaps.directions(origin_location, "ETH Zurich", mode="transit", departure_time=datetime.datetime.now())
+    # write directions to file correclty formatted
+    with open('directions_to_eth.json', 'w') as outfile:
+        json.dump(directions_to_eth, outfile, indent=4, sort_keys=True)
 
 def get_times():
-  with open('directions_to_eth.json') as f:
-      data = json.load(f)
-  legs = data[0]['legs']
-  if legs and len(legs) > 0:
-      leg = legs[0]
-      if leg['steps'] and len(leg['steps']) > 0:
-          for step in leg['steps']:
-              if step['travel_mode'] == 'TRANSIT':
-                  departure_time = step['transit_details']['departure_time']['text']
-                  bus_number = step['transit_details']['line']['short_name']
-                  print(f"Bus {bus_number} departs at {departure_time}")
-      else:
-          print("Steps not found in the first leg.")
-  else:
-      print("Legs not found in the response.")
+    with open('directions_to_eth.json') as f:
+        data = json.load(f)
+    legs = data[0]['legs']
+    if legs and len(legs) > 0:
+        leg = legs[0]
+        if leg['steps'] and len(leg['steps']) > 0:
+            for step in leg['steps']:
+                if step['travel_mode'] == 'TRANSIT':
+                    departure_time = step['transit_details']['departure_time']['text']
+                    bus_number = step['transit_details']['line']['short_name']
+                    print(f"Bus {bus_number} departs at {departure_time}")
+        else:
+            print("Steps not found in the first leg.")
+    else:
+        print("Legs not found in the response.")
 
 def main():
     parser = ArgumentParser()
